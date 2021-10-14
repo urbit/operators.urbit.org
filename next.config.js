c530db93103abb7c5b4abd6d4f8ec9501994bcdb
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-10-14 12:49:12 -0400

    try removing next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-module.exports = {
-  reactStrictMode: false,
-  // target: 'serverless',
-  webpack(config, {}) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      // fs: false is nessecary even for webpack 4
-      fs: false,
-      // the rest is nessesary for webpack 5 in next 11
-      child_process: false,
-      net: false,
-      crypto: false,
-    };
-    return config;
-  },
-};
+// module.exports = {
+//   reactStrictMode: false,
+//   // target: 'serverless',
+//   webpack(config, {}) {
+//     config.resolve.fallback = {
+//       ...config.resolve.fallback,
+//       // fs: false is nessecary even for webpack 4
+//       fs: false,
+//       // the rest is nessesary for webpack 5 in next 11
+//       child_process: false,
+//       net: false,
+//       crypto: false,
+//     };
+//     return config;
+//   },
+// };
