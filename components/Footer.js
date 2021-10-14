6a6a7c2bca42c245b37180f3542d2f38bf3e2e5d
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-10-14 13:28:56 -0400

    add FAQ to footer

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,12 @@ export default function Footer() {
           <a className="type-ui">The Urbit Guidebook</a>
         </Link>
 
-        <a className="type-ui flex" href="https://urbit.org" target="_blank">Urbit.org <div className="font-sans w-8 h-8 rounded-full bg-wall-600 dark:bg-antiwall-600 ml-2 text-wall-100 dark:text-antiwall-100 flex items-center justify-center"><p className="leading-none pt-0.5">{'↗'}</p></div></a>
+        <nav className="flex">
+        <Link href="/faq">
+          <a className="type-ui mr-4">FAQ</a>
+        </Link>
+        <a className="type-ui flex" href="https://urbit.org" target="_blank">urbit.org <div className="font-sans w-8 h-8 rounded-full bg-wall-600 dark:bg-antiwall-600 ml-2 text-wall-100 dark:text-antiwall-100 flex items-center justify-center"><p className="leading-none pt-0.5">{'↗'}</p></div></a>
+        </nav>
     </footer>
   );
 }
