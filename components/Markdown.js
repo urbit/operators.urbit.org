2171e20e29a0ddac161683f0df791c2641d76f7d
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-10-14 13:14:07 -0400

    wip update markdown parsing

--- components/Markdown.js
+++ components/Markdown.js
@@ -2,9 +2,14 @@ import remark from "remark";
 import gfm from "remark-gfm";
 import slug from "remark-slug";
 import heading from "remark-heading-id";
-import html from "remark-html";
 import remarkprism from "remark-prism";
 import normalize from "mdurl/encode";
+import merge from "deepmerge";
+import github from "hast-util-sanitize/lib/github";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeRaw from "rehype-raw";
 
 // img is wrapped in figure so that images can be extra wide in the blog posts
 function Img(h, node) {
@@ -30,19 +35,26 @@ const options = {
   handlers: {
     image: Img,
   },
-  entities: "numbers",
+  sanitize: merge(github, {
+    // remove user-content from github.json to remark-slug work as expected
+    clobberPrefix: "",
+    attributes: { "*": ["className"] },
+  }),
 };
 
 // Converts markdown strings into markdown HTML/React components
 export default async function Markdown({ post }) {
   const result = await remark()
-    .use(html, options)
-    .use(gfm)
+    .use(remarkParse, options)
     .use(remarkprism, {
       plugins: ["show-invisibles"],
     })
+    .use(gfm)
     .use(slug)
     .use(heading)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
     .process(post.content);
 
   return result.toString();
--- next.config.js
+++ next.config.js
@@ -1,16 +1,63 @@
-// module.exports = {
-//   reactStrictMode: false,
-//   // target: 'serverless',
-//   webpack(config, {}) {
-//     config.resolve.fallback = {
-//       ...config.resolve.fallback,
-//       // fs: false is nessecary even for webpack 4
-//       fs: false,
-//       // the rest is nessesary for webpack 5 in next 11
-//       child_process: false,
-//       net: false,
-//       crypto: false,
-//     };
-//     return config;
-//   },
-// };
+const withTM = require("next-transpile-modules")([
+  "remark-parse",
+  "remark-gfm",
+  "remark-rehype",
+  "rehype-stringify",
+  "rehype-raw",
+  "micromark-core-commonmark",
+  "micromark-extension-gfm",
+  "micromark-util-combine-extensions",
+  "micromark-util-chunked",
+  "micromark-util-character",
+  "micromark-util-sanitize-uri",
+  "micromark-util-encode",
+  "micromark-util-classify-character",
+  "micromark-util-resolve-all",
+  "micromark-util-subtokenize",
+  "micromark-util-normalize-identifier",
+  "micromark-util-html-tag-name",
+  "micromark-util-decode-numeric-character-reference",
+  "micromark-util-decode-string",
+  "micromark-factory-destination",
+  "micromark-factory-label",
+  "micromark-factory-space",
+  "micromark-factory-title",
+  "micromark-factory-whitespace",
+  "mdast-util-gfm",
+  "ccount",
+  "mdast-util-find-and-replace",
+  "hast-util-raw",
+  "hast-util-from-parse5",
+  "hast-util-to-parse5",
+  "hast-util-parse-selector",
+  "hast-to-hyperscript",
+  "hastscript",
+  "unist-util-visit-parents",
+  "unist-util-is",
+  "vfile-location",
+  "web-namespaces",
+  "mdast-util-to-markdown",
+  "mdast-util-from-markdown",
+  "markdown-table",
+]);
+
+module.exports = withTM({
+  reactStrictMode: false,
+  // target: 'serverless',
+  webpack5: false,
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+    // config.externals = {
+    //   ...config.externals,
+    //   canvas: "util",
+    //   bufferutil: "bufferutil",
+    //   "utf-8-validate": "utf-8-validate"
+    // }
+    return config;
+  },
+});
