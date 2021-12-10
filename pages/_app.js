6cc7ff75ab32a7325b8256f0674e218013d53d41
Gavin <gavin@macbook-pro.myfiosgateway.com>
2021-12-09 19:14:01 -0500

    add Matamo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+import { init } from "@socialgouv/matomo-next";
+
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "../styles/markdown.css";
 import "../styles/prism.css";
 
+
 function MyApp({ Component, pageProps }) {
-  
+
+  useEffect(() => {
+    init({
+      url: MATOMO_URL,
+      siteId: MATOMO_SITE_ID,
+    });
+  });
 
   return (
     <>
