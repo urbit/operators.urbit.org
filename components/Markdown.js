4e3ac7f765620e5f45c29d17996c4bfda85c6349
Matilde Park <m@park.computer>
2022-05-26 15:18:39 -0700

    markdown: use current urbit.org pipeline

--- components/Markdown.js
+++ components/Markdown.js
@@ -2,12 +2,17 @@ import remark from "remark";
 import gfm from "remark-gfm";
 import slug from "remark-slug";
 import heading from "remark-heading-id";
-import html from "remark-html";
 import remarkprism from "remark-prism";
 import normalize from "mdurl/encode";
+import merge from "deepmerge";
+import github from "hast-util-sanitize/lib/github";
+import remarkParse from "remark-parse";
+import remarkRehype, { defaultHandlers, all } from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeRaw from "rehype-raw";
 
 // img is wrapped in figure so that images can be extra wide in the blog posts
-function Img(h, node) {
+function img(h, node) {
   var props = { src: normalize(node.url), alt: node.alt };
   if (node.title !== null && node.title !== undefined) {
     props.title = node.title;
@@ -26,23 +31,63 @@ function Img(h, node) {
   };
 }
 
+function a(h, node) {
+  const url = node.url;
+
+  if (
+    url.includes("https://operators.urbit.org") ||
+    url.startsWith("/") ||
+    url.startsWith("#")
+  ) {
+    return {
+      type: "element",
+      tagName: "a",
+      properties: {
+        href: url,
+      },
+      children: all(h, node),
+    };
+  } else {
+    return {
+      type: "element",
+      tagName: "a",
+      properties: {
+        target: "_blank",
+        rel: "noopener",
+        href: url,
+      },
+      children: all(h, node),
+    };
+  }
+}
+
+const handlers = defaultHandlers;
+handlers.link = a;
+handlers.image = img;
+
 const options = {
-  handlers: {
-    image: Img,
-  },
-  entities: "numbers",
+  allowDangerousHtml: true,
+  handlers: handlers,
+  sanitize: merge(github, {
+    // remove user-content from github.json to remark-slug work as expected
+    clobberPrefix: "",
+    attributes: { "*": ["className"] },
+  }),
 };
 
 // Converts markdown strings into markdown HTML/React components
-export default async function Markdown({ post }) {
+export default async function Markdown({ post }, disablePlugins) {
   const result = await remark()
-    .use(html, options)
-    .use(gfm)
+    .use(remarkParse)
     .use(remarkprism, {
-      plugins: ["show-invisibles"],
+      plugins: !disablePlugins ? ["show-invisibles"] : [],
     })
+    .use(gfm)
     .use(slug)
     .use(heading)
+    .use(remarkRehype, options)
+    .use(rehypeRaw)
+    .use(rehypeStringify)
     .process(post.content);
 
   return result.toString();
