e658887ce748ee9d6ed9e9a37711a8ecd102a050
Matilde Park <m@park.computer>
2022-10-19 12:07:49 -0700

    api: stub staging developers api

--- pages/api/dev-search.js
+++ pages/api/dev-search.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
   const results = await fetch(
-    `https://developers.urbit.org/api/search?q=${req.query.q}`
+    `https://developers-urbit-bls6d8h8w-urbit.vercel.app/api/search?q=${req.query.q}`
   ).then((res) => res.json());
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
