5dbe2afd3bc3c0c1aea627808e609d47e164ba9a
Matilde Park <m@park.computer>
2022-10-19 13:19:59 -0700

    api: remove staging endpoints

--- pages/api/dev-search.js
+++ pages/api/dev-search.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
   const results = await fetch(
-    `https://developers-urbit-bls6d8h8w-urbit.vercel.app/api/search?q=${encodeURIComponent(req.query.q)}`
+    `https://developers.urbit.org/api/search?q=${encodeURIComponent(req.query.q)}`
   ).then((res) => res.json());
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
